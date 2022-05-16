6987c1c5dd241383bfb9bb7b529585e91dfc2372
saiful Islam <saiful.islam5@northsouth.edu>
2022-05-16 15:50:34 +0600

    Design Menu Item

--- src/component/body/MenuItem.js
+++ src/component/body/MenuItem.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { Card , CardImg ,CardBody , CardTitle , CardImgOverlay } from "reactstrap";
+
+
 const MenuItem =props=>{
-    console.log(props)
+    
     return(
         <div>
+            <Card style={{margin : "15px" }}>
+                <CardBody>
+                    <CardImg width="100%" alt={props.dish.name} src={props.dish.image} style={{opacity :"0.6"}}></CardImg>
+                    <CardImgOverlay>
+                        <CardTitle style={{color : "black", fontSize:"24px"}}>{props.dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </CardBody>
+            </Card>
+
 
         </div>
 
